ce37fe637dc7eb8ef34bf1cb2c25fbcd314fb0c2
Aran Seehra <aranseehra@gmail.com>
2022-11-21 14:40:40 +0000

    generate controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_142912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "timeslot"
+    t.bigint "teachers_id", null: false
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["teachers_id"], name: "index_bookings_on_teachers_id"
+    t.index ["users_id"], name: "index_bookings_on_users_id"
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "location"
+    t.string "bio"
+    t.string "specialty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookings", "teachers", column: "teachers_id"
+  add_foreign_key "bookings", "users", column: "users_id"
 end
