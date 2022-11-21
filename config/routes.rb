ad6978738c4963c361e4937041f00273c6f2fec1
Aran Seehra <aranseehra@gmail.com>
2022-11-21 15:06:11 +0000

    update routes with teachers and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'teachers/index'
-  get 'teachers/show'
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :teachers do
+    resources :bookings
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
