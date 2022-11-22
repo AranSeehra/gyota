f0610012db87bb335d46f8d746c49824e6084df6
Som Akudu <somakudu@gmail.com>
2022-11-22 13:03:59 +0000

    Create dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-  root to: "teachers#index"
-  devise_for :users do
-    resources :pages
-  end
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+root to: "teachers#index"
+devise_for :users
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :teachers do
     resources :bookings
   end
+
+  resources :bookings
+
+  get "dashboard", to: "users#dashboard", as: "dashboard"
 
   # Defines the root path route ("/")
   # root "articles#index"
