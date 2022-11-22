a7028a874212b427126d430ca56243f63b725a2d
Som Akudu <somakudu@gmail.com>
2022-11-22 15:48:45 +0000

    Finished adding details for teacher photo

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -19,6 +19,6 @@ class TeachersController < ApplicationController
   end
 
   def strong_params
-    params.require(:teacher).permit(:first_name, :last_name, :location, :bio, :specialty)
+    params.require(:teacher).permit(:first_name, :last_name, :location, :bio, :specialty, :photo)
   end
 end
