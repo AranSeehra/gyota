d3c41af457a03682891f4e9cbc069d09230124a0
Aran Seehra <aranseehra@gmail.com>
2022-11-22 17:28:41 +0000

    amend datetime into simple form for display in user dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,4 @@
+require 'date'
 class BookingsController < ApplicationController
 
   def show
@@ -11,10 +12,28 @@ class BookingsController < ApplicationController
 
   def create
     @teacher = Teacher.find(params[:teacher_id])
+    year_s = params[:booking]["start_at(1i)"].to_i
+    month_s = params[:booking]["start_at(2i)"].to_i
+    date_s = params[:booking]["start_at(3i)"].to_i
+    hour_s = params[:booking]["start_at(4i)"].to_i
+    minute_s = params[:booking]["start_at(5i)"].to_i
+    start = DateTime.new(year_s, month_s, date_s, hour_s, minute_s)
+
+    year_e = params[:booking]["end_at(1i)"].to_i
+    month_e = params[:booking]["end_at(2i)"].to_i
+    date_e = params[:booking]["end_at(3i)"].to_i
+    hour_e = params[:booking]["end_at(4i)"].to_i
+    minute_e = params[:booking]["end_at(5i)"].to_i
+    endtime = DateTime.new(year_e, month_e, date_e, hour_e, minute_e)
+
     @booking = Booking.new(booking_params)
-    # @teacher.booking = @booking
+    @booking.start_at = start
+    @booking.end_at = endtime
+    @booking.teacher = @teacher
+    @booking.user = current_user
+    # raise
     @booking.save
-    redirect_to dashboard_path
+    redirect_to dashboard_path(current_user)
   end
 
   def edit
@@ -39,6 +58,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:timeslot, :start_at, :end_at, :teacher_id, :user_id)
+    params.require(:booking).permit(:start_at, :end_at, :teacher_id, :user_id)
   end
 end
