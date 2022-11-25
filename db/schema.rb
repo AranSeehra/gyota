14b228498db943cd502004fde88c9f5ccc0226a2
Ariel Lima <ariel_lima_oliveira@yahoo.com.br>
2022-11-25 14:36:08 +0000

    front end

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_11_24_165641) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,9 +71,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_165641) do
     t.string "specialty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price"
     t.float "latitude"
     t.float "longitude"
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
